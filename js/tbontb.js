acd576500f8e05273f7e583f3b0a09622d960a8d
onp <onptrs@gmail.com>
2014-02-21 16:50:13 -0500

    forces to keep nodes on screen, reheat on resize

--- js/tbontb.js
+++ js/tbontb.js
@@ -1,16 +1,23 @@
 var storyData
+var fieldWidth
+var fieldHeight
 
 var createGraph = function(){
     var storyBox = d3.select("#story-box")
+    fieldWidth = storyBox.style("width").slice(0,-2)
+    fieldHeight = storyBox.style("height").slice(0,-2)
     var force = d3.layout.force()
         .nodes(storyData.pages)
         .links(storyData.links)
         .linkDistance(30)
-        .size([storyBox.style("width").slice(0,-2),storyBox.style("height").slice(0,-2)])
+        .size([fieldWidth,fieldHeight])
         .start();
 
     window.addEventListener("resize",function(){
-        force.size([storyBox.style("width").slice(0,-2),storyBox.style("height").slice(0,-2)]);
+        fieldWidth = storyBox.style("width").slice(0,-2)
+        fieldHeight = storyBox.style("height").slice(0,-2)
+        force.size([fieldWidth,fieldHeight]);
+        force.resume()
     })
         
     var visLinks = storyBox.selectAll(".link")
@@ -31,6 +38,17 @@ var createGraph = function(){
             .call(force.drag)
     
     force.on("tick",function(){
+        storyData.pages.forEach(function(o,i){
+            if (o.x < 1){o.x = 1}
+            if (o.x < 20){o.x += 3/o.x}
+            if (o.y < 1){o.y = 1}
+            if (o.y < 20){o.y += 3/o.y}
+            if (o.x > (fieldWidth - 1)){o.x = fieldWidth - 1}
+            if (o.x > (fieldWidth - 20)){o.x -= 3/(fieldWidth - o.x)}
+            if (o.y > (fieldHeight - 1)){o.y = fieldHeight - 1}
+            if (o.y > (fieldHeight - 20)){o.y -= 3/(fieldHeight - o.y)}
+            
+        })
         visLinks.attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
